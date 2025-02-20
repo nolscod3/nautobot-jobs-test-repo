6ac04536aec5bb9d4795856252a27b1aa57e8ef5
nolscod3 <139678325+nolscod3@users.noreply.github.com>
2025-02-20 20:45:40 +1000

    Update hello_jobs.py

--- jobs/hello_jobs.py
+++ jobs/hello_jobs.py
@@ -1,1 +1,8 @@
+from nautobot.apps.jobs import Job
 
+class HelloJobs(Job):
+
+  class Meta:
+    name = "Hello Jobs from Git Repo"
+  def run(self):
+    self.logger.debug("This is from the Git repo.")
